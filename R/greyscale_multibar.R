98646e33b95a3af4d009a7768796d02e2c631876
Eugenia Gabrielova <genia.likes.science@gmail.com>
2015-03-12 19:53:24 -0700

    updates some R scripts, adds summary script

--- R/greyscale_multibar.R
+++ R/greyscale_multibar.R
@@ -1,7 +1,7 @@
 require(ggplot2)
 
 greyscale_multibar <- function(dataframe, title, xlabel, ylabel, aux=NULL) {
-  p <- ggplot(data=dataframe, aes(x=Bots, y=Y, fill=Events)) + 
+  p <- ggplot(data=dataframe, aes(y=Y)) + 
     geom_bar(stat="identity",position=position_dodge(),colour="black") +
     geom_errorbar(aes(ymin=Y-Error, ymax=Y+Error), width=.2, position=position_dodge(.9)) +
     scale_fill_grey(start = 0.6, end = 0.2, na.value = "grey50") +
--- condensed.R
+++ condensed.R
@@ -1,53 +1,58 @@
+library(ggplot2)
+library(plyr)
+
 # Helper functions
-source("~/Code/R-me-hearties/R/botscale_dataframe.R")
-source("~/Code/R-me-hearties/R/greyscale_multibar.R")
 source("~/Code/R-me-hearties/R/multiplot.R")
-se <- function(x) sd(x)/sqrt(length(x))
+source("~/Code/R-me-hearties/R/summarySE.R")
 
-# Read log files
-cpu_log = read.csv("~/Code/R-me-hearties/data/mar-14/cpu.csv") 
-processed_log = read.csv("~/Code/R-me-hearties/data/mar-14/processed.csv")
-total_inc_log = read.csv("~/Code/R-me-hearties/data/mar-14/incoming_total.csv")
-total_out_log = read.csv("~/Code/R-me-hearties/data/mar-14/outgoing.csv")
+# Factors
+regionfactors = c("Light Scene\nLight Avatar", "Light Scene\nHeavy Avatar", "Heavy Scene\nLight Avatar", "Heavy Scene\nHeavy Avatar")
+breakthing = c(0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150)
 
-# Plot titles and labels
-cpu_title = "% CPU Usage of Sitting OpenSimulator Bots"
-processed_title = "Incoming Packets Processed by Region Server"
-total_inc_title = "Average Packets Received by Region Server"
-total_out_title = "Average Packets Sent by Region Server"
-
-bot_xlabel = "Number of Bots"
-cpu_ylabel = "% CPU Usage"
-packets_ylabel = "Average packets/s"
-
-# Means and errors of data
-cpu_means = colMeans(cpu_log)
-cpu_errors = apply(cpu_log, 2, se)
-processed_means = colMeans(processed_log)
-processed_errors = apply(processed_log, 2, se)
-total_inc_means = colMeans(total_inc_log)
-total_inc_errors = apply(total_inc_log, 2, se)
-total_out_means = colMeans(total_out_log)
-total_out_errors = apply(total_out_log, 2, se)
-
-# Create dataframes
-cpu_frame = botscale_dataframe(cpu_means, cpu_errors)
-processed_frame = botscale_dataframe(processed_means, processed_errors)
-total_inc_frame = botscale_dataframe(total_inc_means, total_inc_errors)
-total_out_frame = botscale_dataframe(total_out_means, total_out_errors)
-
-# CPU
-cpu_plot = greyscale_multibar(cpu_frame, cpu_title, bot_xlabel, cpu_ylabel)
-#print(cpu_plot)
-
-# Baseline Histogram
-m <- ggplot(cpu_log[c(2)], aes(x=baseline_sitting_50)) 
-m + geom_histogram() + ylab("count") + xlab("Average CPU load") + ggtitle("Distribution of CPU Load for 50 Bots, Baseline Configuration")
+# Data - Scenes
+light_sce_region_serv = c()
+heavy_sce_region_serv = c()
+light_sce_region_serv_std = c() 
+heavy_sce_region_serv_std = c()
   
-# Packets Processed, use Multiplot
-cpu_plot = greyscale_multibar(cpu_frame, cpu_title, bot_xlabel, cpu_ylabel)
-processed_plot = greyscale_multibar(processed_frame, processed_title, bot_xlabel, packets_ylabel, aux=theme(legend.position="none"))
-total_inc_plot = greyscale_multibar(total_inc_frame, total_inc_title, bot_xlabel, packets_ylabel, aux=theme(legend.position="none"))
-total_out_plot = greyscale_multibar(total_out_frame, total_out_title, bot_xlabel, packets_ylabel)
+# Data - Avatars
 
-#multiplot(total_inc_plot, processed_plot, total_out_plot, cols=3)
+# Data - Inventories
+light_inv_region_serv = c(18.75, 24.58, 26.30, 32.01)
+light_inv_robust_serv = c(16.10, 20.55, 26.18, 30.65)
+heavy_inv_region_serv = c(134.39, 102.75, 77.86, 103.59)
+heavy_inv_robust_serv = c(14.90, 20.52, 28.10, 32.49)
+
+# Standard Deviations - Inventories
+light_inv_region_serv_std = c(0.29, 0.71, 0.32, 0.60)
+light_inv_robust_serv_std = c(0.39, 0.25, 0.19, 0.47)
+heavy_inv_region_serv_std = c(2.83, 42.55, 12.95, 12.65)
+heavy_inv_robust_serv_std = c(0.85, 0.81, 0.24, 0.72)
+
+lightInvDF = data.frame(Configs=factor(regionfactors, levels=regionfactors),
+                        Servers=factor(c(rep("Simulation Server",4),rep("Dedicated Server",4)), levels=c("Simulation Server", "Dedicated Server")),
+                        Y=c(light_inv_region_serv,light_inv_robust_serv),
+                        Error=c(light_inv_region_serv_std,light_inv_robust_serv_std))
+
+heavyInvDF = data.frame(Configs=factor(regionfactors, levels=regionfactors),
+                        Servers=factor(c(rep("Simulation Server",4),rep("Dedicated Server",4)), levels=c("Simulation Server", "Dedicated Server")),
+                        Y=c(heavy_inv_region_serv,heavy_inv_robust_serv),
+                        Error=c(heavy_inv_region_serv_std,heavy_inv_robust_serv_std))
+
+lip <- ggplot(data=lightInvDF, aes(x=Configs,y=Y,fill=Servers)) + 
+  geom_bar(width=.5, stat="identity",position="dodge") + 
+  scale_fill_manual(name="Item retrieval from", values=c("#5D92B1", "#9BCD9B")) +
+  xlab("") + ylab("Total Processor Time (s)") + ggtitle("Light User Inventory\nPrivileged Processor Time") +
+  geom_errorbar(aes(ymin=Y-Error, ymax=Y+Error), width=.2, position=position_dodge(.5)) +
+  theme(axis.text.x = element_text(colour = 'black'), axis.text.y = element_text(colour='black'), text = element_text(size=16)) + 
+  scale_y_continuous(limits=c(0,150),breaks=breakthing) + theme(legend.position="top")
+
+hip = ggplot(data=heavyInvDF, aes(x=Configs,y=Y,fill=Servers)) + 
+  geom_bar(width=.5, stat="identity",position="dodge") + 
+  scale_fill_manual(name="Item retrieval from", values=c("#5D92B1", "#9BCD9B")) +
+  xlab("") + ylab("Total Processor Time (s)") + ggtitle("Heavy User Inventory\nPrivileged Processor Time") +
+  geom_errorbar(aes(ymin=Y-Error, ymax=Y+Error), width=.2, position=position_dodge(.5)) +
+  theme(axis.text.x = element_text(colour = 'black'), axis.text.y = element_text(colour='black'), text = element_text(size=16)) + 
+  scale_y_continuous(limits=c(0,150),breaks=breakthing) + theme(legend.position="top")
+
+multiplot(lip,hip,cols=2)
